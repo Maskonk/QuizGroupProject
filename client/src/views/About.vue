9b889464ab9f77c73f6bba65842c87d22b25c647
Tyr <mtyran@gmail.com>
2019-07-05 14:16:29 +0100

    created home+about

---
+++ client/src/views/About.vue
@@ -0,0 +1,15 @@
+<template lang="html">
+<div class="">
+</div>
+</template>
+
+<script>
+import Answer from "@/components/Answer"
+
+export default {
+  props: []
+}
+</script>
+
+<style lang="css" scoped>
+</style>
