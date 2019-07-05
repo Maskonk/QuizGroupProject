2aff554adf6c0c03bd06ab13cf9a388c38083ee2
John McNeill <Shetlandjohnmcneill@hotmail.co.uk>
2019-07-05 13:19:35 +0100

    I think this is all i can do for now

---
+++ client/src/components/QuizQuestion.vue
@@ -0,0 +1,23 @@
+<!-- this is all i can do untill questions prop is created  -->
+
+
+<template lang="html">
+<h1>Question</h1>
+<input type="radio" id="one" value="" v-model='question-answear'>
+<input type="radio" id="two" value="" v-model='question-answear'>
+<input type="radio" id="three" value="" v-model='question-answear'>
+<input type="radio" id="four" value="" v-model='question-answear'>
+</template>
+
+
+<script>
+export default {
+  name: 'quiz-question',
+  data(){
+    'question-answer': null
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
