998aa3c58d95cf34a11df851eded2077603c0129
Rory <ruaidhiri@gmail.com>
2019-07-07 19:53:30 +0100

    Changed nav items to list and added CSS

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -1,10 +1,12 @@
 <template lang="html">
-  <nav>
-    <router-link :to="{ name: '', params: {} }">Homepage</router-link>
-    <router-link :to="{ name: '', params: {} }">Learn</router-link>
-    <router-link :to="{ name: '', params: {} }">Quiz</router-link>
-    <router-link :to="{ name: '', params: {} }">Score</router-link>
-    <router-link :to="{ name: '', params: {} }">About</router-link>
+  <nav class="navbar">
+    <ul>
+      <li><router-link :to="{ name: '', params: {} }">HOME</router-link></li>
+      <li><router-link :to="{ name: '', params: {} }">LEARN</router-link></li>
+      <li><router-link :to="{ name: '', params: {} }">QUIZ</router-link></li>
+      <li><router-link :to="{ name: '', params: {} }">SCORE</router-link></li>
+      <li class="about"><router-link :to="{ name: '', params: {} }">ABOUT</router-link></li>
+    </ul>
   </nav>
 </template>
 
@@ -14,4 +16,40 @@ export default {
 </script>
 
 <style lang="css" scoped>
+
+/* Remove bullet points from list items */
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+}
+
+/* background colour */
+.navbar {
+  background-color: black;
+  overflow: hidden;
+}
+
+/* Style the links inside the navigation bar */
+.navbar a {
+  font-family: helvetica;
+  float: left;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+/* Align about to right side */
+.about {
+  float: right;
+}
+
+/* Colour changes with hover */
+.navbar a:hover {
+  background-color: white;
+  color: black;
+}
+
 </style>
