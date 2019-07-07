56e9e700048d1944342ad58921e81df874fafc08
Kyle Walker <Randomer44@hotmail.com>
2019-07-08 00:08:08 +0100

    Renamed result.vue to Score.vue to match entry in router.js

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -1,9 +1,9 @@
 <template lang="html">
   <nav>
     <router-link :to="{ name: 'home' }">Homepage</router-link>
-    <!-- <router-link :to="{ name: 'learn' }">Learn</router-link>
-    <router-link :to="{ name: 'quiz' }">Quiz</router-link>
-    <router-link :to="{ name: 'score' }">Score</router-link> -->
+<!--    <router-link :to="{ name: 'learn' }">Learn</router-link>-->
+<!--    <router-link :to="{ name: 'quiz' }">Quiz</router-link>-->
+    <router-link :to="{ name: 'score' }">Score</router-link>
     <router-link :to="{ name: 'about' }">About</router-link>
   </nav>
 </template>
