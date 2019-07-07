35f611054f1e793f8edefb81709027912a087ede
John McNeill <Shetlandjohnmcneill@hotmail.co.uk>
2019-07-07 19:22:34 +0100

    Got the random select function to work, going to wait till tommorow to merge in case of any merge conflicts

--- client/src/components/Questions.vue
+++ client/src/components/Questions.vue
@@ -9,32 +9,43 @@ export default {
   // props: ['questions']
   data(){
     return {
-    questionsToAsk:[],
-    questions_test:[
-      {question: 'what day is it',
-      answear: 'today'},
-      {question: 'is this hard to do?',
-      answear: 'yes'},
-      {question: 'how hot is it day',
-      answear: '18C'}
+      questionsToAsk:[],
+      questions_test:[
+        {question: 'what day is it',
+        answear: 'today'},
+        {question: 'is this hard to do?',
+        answear: 'yes'},
+        {question: 'how hot is it day',
+        answear: '18C'}
       ]
     };
   },
   methods: {
     getQuestions(){
-    var numbQuestions = 5
-    for(n=0, n > numbQuestions, n++)
-      var filteredArray =
-        this.questions_test.filter(function(item, pos){
-        return this.questions_test.indexOf(item) === pos;
-      })
+      const numberOfQuestionsSelected = 2 //this will be a v-bound property that the user inputs at on point
+      var shuffledQuestions = this.shuffleArray(this.questions_test);
+      var randomQuestions = shuffledQuestions.slice(0, numberOfQuestionsSelected);
+      return this.questionsToAsk = randomQuestions;
+    },
+    shuffleArray(array){
+      var counter = array.length;
+      while (counter > 0 ){
+        let index = Math.floor(Math.random() * counter);
+        counter = counter - 1;
+        let temp = array[counter];
+        array[counter] = array[index];
+        array[index] = temp;
+      }
+      return array;
     }
+  },
+  mounted(){
+    this.getQuestions();
   }
 }
 
-var filteredArray = arr.filter(function(item, pos){
-  return arr.indexOf(item)== pos;
-</script>
 
-<style lang="css" scoped>
-</style>
+  </script>
+
+  <style lang="css" scoped>
+  </style>
