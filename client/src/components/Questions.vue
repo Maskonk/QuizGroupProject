275dddca16f37fcb15defb37fb47dd92f9dd0b16
Kyle Walker <Randomer44@hotmail.com>
2019-07-08 12:33:37 +0100

    Limited it to 10 of the 20 random questions.:

--- client/src/components/Questions.vue
+++ client/src/components/Questions.vue
@@ -1,7 +1,7 @@
 <template lang="html">
   <div>
     <h4>{{question.question}}</h4>
-    <answer :question="question.question" :answers="question.questionOption"></answer>
+    <answer :question="question.question" :answers="question.questionOption" v-model="apples"/>
   </div>
 
 </template>
@@ -14,35 +14,8 @@ export default {
   props: ['question'],
   data(){
     return {
-      questionsToAsk:[],
-      questions_test:[
-        {question: 'what day is it',
-        answear: 'today'},
-        {question: 'is this hard to do?',
-        answear: 'yes'},
-        {question: 'how hot is it day',
-        answear: '18C'}
-      ]
+      apples: []
     };
-  },
-  methods: {
-    getQuestions(){
-      const numberOfQuestionsSelected = 2; //this will be a v-bound property that the user inputs at on point
-      var shuffledQuestions = this.shuffleArray(this.questions_test);
-      var randomQuestions = shuffledQuestions.slice(0, numberOfQuestionsSelected);
-      return this.questionsToAsk = randomQuestions;
-    },
-    shuffleArray(array){
-      var counter = array.length;
-      while (counter > 0 ){
-        let index = Math.floor(Math.random() * counter);
-        counter = counter - 1;
-        let temp = array[counter];
-        array[counter] = array[index];
-        array[index] = temp;
-      }
-      return array;
-    }
   },
   mounted(){
     this.getQuestions();
