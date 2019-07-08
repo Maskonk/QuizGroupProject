5ec93329021d73ef6bd0ae954c79485e30e48184
Rory <ruaidhiri@gmail.com>
2019-07-08 13:04:44 +0100

    About tidy-up

--- client/src/views/Info.vue
+++ client/src/views/Info.vue
@@ -1,32 +1,19 @@
 <template>
   <div id="main">
-    <h3>A breif introduction to the life of Ghenis Khan </h3>
-    <p>Genghis Khan is a famous Mongol leader and warrior, who lived in the 12th and 13th centuries. He is most well-known for uniting the nomadic tribes of Mongolia and then leading those tribes into many battles; eventually conquering a large part of Asia and Europe.
+    <h3>A brief introduction to the life of Genghis Khan </h3>
+    <p>Genghis Khan was a famous Mongol leader and warrior who lived in the 12th and 13th centuries. He is most well-known for uniting the nomadic tribes of Mongolia before leading those tribes into many battles, eventually conquering a large part of Asia and Europe.
 <br>
 <br>
-He was born born around 1162, into an important family in a Mongolian tribe. His birth place is near modern-day Ulaanbaatar, the capital of Mongolia. According to apocryphal texts he was born with a blood clot clenched in his fist; a sign that he was going to grow up to be a powerful leader. He was born with the name Temüjin, which means Iron worker. He gained the game Genghis, meaning universe ruler, through his military campaigns. He had four wives throughout his life, and a total of four sons
-<br>
-<br>
-At its peak, the Mongolian empire was the largest contiguous land empire in human history. In the east it reached as far as Korea and all of modern China, and to the west it reached into countries that are now part of eastern Europe and into Finland. This massive expansion – from nothing to over ten million square miles – happened mostly in Genghis Khan’s lifetime, and was largely due to his extremely aggressive military tactics. While the growth of the empire was swift, it’s fall was also fast. Genghis Khan’s grandchildren split the empire between them, ending the united Mongolian empire.
-<br>
-<br>
-Genghis Khan is a controversial figure. Some praise him for his military prowess. Indeed, the Mongols were an incredible fighting force, easily overpowering their foes with their archery and horsemanship skills. It would have been remarkable to have even visited such a large part of the world in the time of Genghis Khan, let alone visit and fully conquer such a large area. Others praise his leadership, saying he was religiously tolerant, lowered taxes and allowed travel and trade.
-<br>
-<br>
-However, the brutality of Genghis Khan cannot be overstated. The Mongol army regularly slaughtered entire villages. It’s believed that the fighting that Genghis started killed up to 40 million people; almost 10% of the population of the world at the time. In the Iranian plateau, up to three quarters of all the people who lived there were killed, and it’s taken until the 20th century for the population to recover. All this killing was done simply to expand the Mongolian empire.
-Genghis Khan is a famous Mongol leader and warrior, who lived in the 12th and 13th centuries. He is most well-known for uniting the nomadic tribes of Mongolia and then leading those tribes into many battles; eventually conquering a large part of Asia and Europe.
-<br>
-<br>
-He was born born around 1162, into an important family in a Mongolian tribe. His birth place is near modern-day Ulaanbaatar, the capital of Mongolia. According to apocryphal texts he was born with a blood clot clenched in his fist; a sign that he was going to grow up to be a powerful leader. He was born with the name Temüjin, which means Iron worker. He gained the game Genghis, meaning universe ruler, through his military campaigns. He had four wives throughout his life, and a total of four sons
+He was born born around 1162 into an important family in a Mongolian tribe. His birth place is near modern-day Ulaanbaatar, the capital of Mongolia. According to apocryphal texts he was born with a blood clot clenched in his fist - a sign that he would grow up to be a powerful leader. He was born with the name Temüjin, which means Iron worker. He gained the game Genghis, meaning universe ruler, through his military campaigns. He had four wives throughout his life, and a total of four sons.
 <br>
 <br>
 At its peak, the Mongolian empire was the largest contiguous land empire in human history. In the east it reached as far as Korea and all of modern China, and to the west it reached into countries that are now part of eastern Europe and into Finland. This massive expansion – from nothing to over ten million square miles – happened mostly in Genghis Khan’s lifetime, and was largely due to his extremely aggressive military tactics. While the growth of the empire was swift, it’s fall was also fast. Genghis Khan’s grandchildren split the empire between them, ending the united Mongolian empire.
 <br>
 <br>
-Genghis Khan is a controversial figure. Some praise him for his military prowess. Indeed, the Mongols were an incredible fighting force, easily overpowering their foes with their archery and horsemanship skills. It would have been remarkable to have even visited such a large part of the world in the time of Genghis Khan, let alone visit and fully conquer such a large area. Others praise his leadership, saying he was religiously tolerant, lowered taxes and allowed travel and trade.
+Genghis Khan is a controversial figure. Some praise him for his military prowess. Indeed, the Mongols were an incredible fighting force, easily overpowering their foes with their archery and horsemanship skills. It would have been remarkable to have even visited such a large part of the world in the time of Genghis Khan, let alone visit and fully conquer such a large area. Others praise his leadership, saying he was religiously tolerant, as well as lowering taxes and allowing travel and trade.
 <br>
 <br>
-However, the brutality of Genghis Khan cannot be overstated. The Mongol army regularly slaughtered entire villages. It’s believed that the fighting that Genghis started killed up to 40 million people; almost 10% of the population of the world at the time. In the Iranian plateau, up to three quarters of all the people who lived there were killed, and it’s taken until the 20th century for the population to recover. All this killing was done simply to expand the Mongolian empire.</p>
+However, the brutality of Genghis Khan cannot be overstated. The Mongol army regularly slaughtered entire villages. It is believed that the fighting that Genghis started killed up to 40 million people - almost 10% of the population of the world at the time. In the Iranian plateau, up to three quarters of all the people who lived there were killed, and it took until the 20th century for the population to recover. All this killing was done simply to expand the Mongolian empire.</p>
   </div>
 </template>
 
