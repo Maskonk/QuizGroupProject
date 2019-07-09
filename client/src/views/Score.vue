29379c5b8fb39f053d936314b170665c920112ca
Kyle Walker <Randomer44@hotmail.com>
2019-07-09 12:20:58 +0100

    Added correct and incorrect labels in score

--- client/src/views/Score.vue
+++ client/src/views/Score.vue
@@ -5,8 +5,8 @@
         <h3>Correct Answers:</h3>
         <ol>
             <div v-for="(question, index) in answers" :question="question" :key="index">
-          <li v-if="question.userAnswer === question.correctAnswer">{{index}}<br>Correct Answer: {{question.correctAnswer}}<br><br></li>
-          <li v-else>{{index}}<br>Your answer: {{question.userAnswer}}<br>Correct Answer: {{question.correctAnswer}}<br><br></li>
+          <li v-if="question.userAnswer === question.correctAnswer">{{index}} <span style="color: green">Correct</span><br>Correct Answer: {{question.correctAnswer}}<br><br></li>
+          <li v-else>{{index}} <span style="color: red">Incorrect</span><br>Your answer: {{question.userAnswer}}<br>Correct Answer: {{question.correctAnswer}}<br><br></li>
             </div>
         </ol>
         </div>
